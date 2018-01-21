de452da7b4732ce00f141969d9abaa846ef5afb0
Anthony Kuske <anthonykuske@gmail.com>
2018-01-21 01:20:54 +0000

    git status

--- src/Libraries/Laravel/Auth/Passwords/PasswordResetTokenRepository.php
+++ src/Libraries/Laravel/Auth/Passwords/PasswordResetTokenRepository.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Tmd\LaravelSite\Libraries\Laravel\Auth\Passwords;
+namespace Amirite\Libraries\Laravel\Auth\Passwords;
 
 use Carbon\Carbon;
 use DB;
 use Illuminate\Auth\Passwords\TokenRepositoryInterface;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Tmd\LaravelSite\Libraries\Traits\GeneratesTokensTrait;
-use Tmd\LaravelSite\Models\User;
 
 class PasswordResetTokenRepository implements TokenRepositoryInterface
 {
@@ -22,30 +22,23 @@ class PasswordResetTokenRepository implements TokenRepositoryInterface
     protected $expires = 3600;
 
     /**
-     * @var string
-     */
-    protected $hashKey;
-
-    /**
      * Create a new token.
      *
-     * @param  \Illuminate\Contracts\Auth\CanResetPassword|User $user
+     * @param \Illuminate\Contracts\Auth\CanResetPassword|Authenticatable $user
      *
      * @return string
      */
     public function create(CanResetPasswordContract $user)
     {
-        $this->deleteExisting($user);
-
         // We will create a new, random token for the user so that we can e-mail them
         // a safe link to the password reset form. Then we will insert a record in
         // the database so that we can verify the token within the actual reset.
-        $token = $this->createNewToken();
+        $token = $this->generateToken();
 
         $this->getTable()->insert(
             [
                 'token' => $token,
-                'userId' => $user->id,
+                'userId' => $user->getAuthIdentifier(),
                 'createdAt' => new Carbon,
             ]
         );
@@ -54,76 +47,52 @@ class PasswordResetTokenRepository implements TokenRepositoryInterface
     }
 
     /**
-     * @param string $token
-     *
-     * @return object|null
-     */
-    public function find($token)
-    {
-        return $this->getTable()->where('token', $token)->first();
-    }
-
-    /**
-     * Delete all existing reset tokens from the database.
-     *
-     * @param  \Illuminate\Contracts\Auth\CanResetPassword|User $user
-     *
-     * @return int
-     */
-    protected function deleteExisting(CanResetPasswordContract $user)
-    {
-        return $this->getTable()->where('userId', $user->id)->delete();
-    }
-
-    /**
      * Determine if a token record exists and is valid.
      *
-     * @param  \Illuminate\Contracts\Auth\CanResetPassword|User $user
-     * @param  string                                           $token
+     * @param \Illuminate\Contracts\Auth\CanResetPassword|Authenticatable $user
+     * @param string $token
      *
      * @return bool
      */
     public function exists(CanResetPasswordContract $user, $token)
     {
-        $token = $this->getTable()->where('userId', $user->id)->where('token', $token)->first();
+        $token = $this->getTable()->where('userId', $user->getAuthIdentifier())->where('token', $token)->first();
 
-        return $token && !$this->tokenExpired($token);
-    }
-
-    /**
-     * Determine if the token has expired.
-     *
-     * @param  object $token
-     *
-     * @return bool
-     */
-    protected function tokenExpired($token)
-    {
-        $expiresAt = Carbon::parse($token->createdAt)->addSeconds($this->expires);
-
-        return $expiresAt->isPast();
+        return $token && !$this->isTokenExpired($token);
     }
 
     /**
      * Delete a token record.
      *
-     * @param CanResetPasswordContract|User $user
+     * @param \Illuminate\Contracts\Auth\CanResetPassword|Authenticatable $user
      */
     public function delete(CanResetPasswordContract $user)
     {
-        $this->getTable()->where('userId', $user->id)->delete();
+        $this->getTable()->where('userId', $user->getAuthIdentifier())->delete();
     }
 
     /**
      * Delete expired tokens.
-     *
-     * @return void
      */
     public function deleteExpired()
     {
         $expiredAt = Carbon::now()->subSeconds($this->expires);
 
         $this->getTable()->where('createdAt', '<', $expiredAt)->delete();
+    }
+
+    /**
+     * Determine if the token has expired.
+     *
+     * @param object $tokenData
+     *
+     * @return bool
+     */
+    protected function isTokenExpired($tokenData)
+    {
+        $expiresAt = Carbon::parse($tokenData->createdAt)->addSeconds($this->expires);
+
+        return $expiresAt->isPast();
     }
 
     /**
--- src/Libraries/Traits/GeneratesTokensTrait.php
+++ src/Libraries/Traits/GeneratesTokensTrait.php
@@ -2,26 +2,25 @@
 
 namespace Tmd\LaravelSite\Libraries\Traits;
 
-use Config;
 use Illuminate\Support\Str;
 
 trait GeneratesTokensTrait
 {
     /**
-     * @return string
-     */
-    protected function getHashKey()
-    {
-        return Config::get('app.key');
-    }
-
-    /**
      * Create a new token string.
      *
      * @return string
      */
-    protected function createNewToken()
+    protected function generateToken(): string
     {
         return hash_hmac('sha256', Str::random(40), $this->getHashKey());
+    }
+
+    /**
+     * @return string
+     */
+    private function getHashKey()
+    {
+        return config('app.key');
     }
 }
